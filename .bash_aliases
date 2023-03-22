550d6b8ac87c101df10b5b6647426e3904b2c9cb
Shiyaam Sunder <shiyaamsunder@users.noreply.github.com>
2023-03-22 08:36:02 +0530

    initial commit

---
+++ .bash_aliases
@@ -0,0 +1,6 @@
+
+# Git
+alias ga='git add'
+alias gc='git commit'
+alias gl='git log --pretty=oneline'
+alias glg='git log --pretty=format:"%h %s" --graph'
---
+++ .local/bin/util.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+languages=`echo "java javascript typescript python go c c++ c#" | tr ' ' '\n'`
+coreutils=`echo "xargs find mv grep sed awk tmux man" | tr ' ' '\n'`
+
+selected=`printf "$languages\n$coreutils" | fzf`
+
+read -p "query: " query
+
+
+if printf $languages | grep -qs $selected; then
+  curl cht.sh/$selected/`echo $query | tr ' ' '+'`
+else
+  curl cht.sh/$selected~$query
+fi
